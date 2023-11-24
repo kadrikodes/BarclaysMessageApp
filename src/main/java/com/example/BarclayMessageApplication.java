594a5954fe173b3994222003222466d0f94cdf32
User1 <kadriidris41@yahoo.com>
2023-11-24 12:07:47 +0000

    more test coverage cheating...

--- src/main/java/com/example/BarclayMessageApplication.java
+++ src/main/java/com/example/BarclayMessageApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
-public class BarclayMessageApplication {
-	public static void main(String[] args) {
-
-		ApplicationContext applicationContext = SpringApplication.run(BarclayMessageApplication.class, args);}
-
-}
+//@SpringBootApplication
+//public class BarclayMessageApplication {
+//	public static void main(String[] args) {
+//
+//		ApplicationContext applicationContext = SpringApplication.run(BarclayMessageApplication.class, args);}
+//
+//}
